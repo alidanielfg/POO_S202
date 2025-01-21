fdc5d64fc290847a1cf57d025f7238a3905be4e4
alidanielfg <alidan2.3fg@gmail.com>
2025-01-21 15:45:24 -0600

    Escribi mi nomber

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-            System.out.println("Hola, Ali Flores");
+            System.out.println("Hola, Ali Daniel Flores");
         }
     }
