b3c2d0673b4d599447146125156c8673d1e80e34
alidanielfg <alidan2.3fg@gmail.com>
2025-01-28 15:08:40 -0600

    Se realizo la suma de los numeros anteriores de un numero entero

---
+++ suma de un numero/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+
+        System.out.println("Ingresa un numero: ");
+        int numero= scn.nextInt();
+
+        int total=0;
+        for (int i=1; i<=numero; i++){
+            total+=i;
+            System.out.println("La suma de "+ total + " + "+ i);
+        }
+        System.out.println("El resultado de la suma es: "+ total);
+    }
+}
