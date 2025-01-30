bce6ba7f27887df8434ca0822a2c655fd1d8c858
alidanielfg <alidan2.3fg@gmail.com>
2025-01-30 14:24:17 -0600

    projecto4 ejercicio2

--- practica4/src/ejercicio2.java
+++ practica4/src/ejercicio2.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class ejercicio2 {
+    public static void main(String[] args){
+        Scanner scn= new Scanner(System.in);
+
+        System.out.println("Ingrese in numero: ");
+        int num= scn.nextInt();
+
+        if(num%2==0){
+            System.out.println("El numero es par");
+        }
+        else{
+            System.out.println("El numero es impar");
+        }
+    }
 }
